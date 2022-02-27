2af7a74c70b55a79e8085d17575186eb0229e65c
전은형 <ooooo5h91@gmail.com>
2022-02-27 20:37:42 +0900

    127.0.0.1로 접속하면 HOME함수에 적혀있는 내용이 실행됨

--- studybud/urls.py
+++ studybud/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('홈페이지')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]

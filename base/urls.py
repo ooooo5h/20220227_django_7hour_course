75ba6e8fd90c8777a3ae065cc3785c6941aedeef
전은형 <ooooo5h91@gmail.com>
2022-02-28 23:06:53 +0900

    path에 room관련 변수를 받게끔 코드 추가

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [   # path(경로, view의 기능, 참조할 이름)
     path('', views.home, name='home'),
-    path('room/', views.room, name='room')
+    path('room/<str:pk>/', views.room, name='room')
 ]

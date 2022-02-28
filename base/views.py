8b65305c96adc8eb7625ed1afbc767d88dde33a2
전은형 <ooooo5h91@gmail.com>
2022-02-28 22:58:46 +0900

    넘겨줄 데이타를 생성하고, home템플릿에 전달해줌

--- base/views.py
+++ base/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name':"Let's learn python"},
+    {'id':2, 'name':"Desing with me"},
+    {'id':3, 'name':"Frontend developters"},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms' : rooms})
 
 def room(request):
     return render(request, 'room.html')

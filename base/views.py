cca084330f2ca3eda4cedaf9ae188ca36a92fdd4
전은형 <ooooo5h91@gmail.com>
2022-03-01 17:47:22 +0900

    특정 단어가 포함되어있는 정보가 결과로 나타남

--- base/views.py
+++ base/views.py
@@ -12,8 +12,8 @@ from .forms import RoomForm
 
 
 def home(request):
-    q = request.GET.get('q')
-    rooms = Room.objects.filter(topic__name = q)
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains = q)
     topics = Topic.objects.all()
     
     context = {'rooms' : rooms, 'topics' : topics}

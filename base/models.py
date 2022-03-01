182d236b68713aeff810bcc6052ae7ec33453091
전은형 <ooooo5h91@gmail.com>
2022-03-01 12:45:54 +0900

    오타 수정

--- base/models.py
+++ base/models.py
@@ -1,4 +1,3 @@
-from pickle import TRUE
 from django.db import models
 
 # Create your models here.
@@ -8,7 +7,7 @@ class Room(models.Model):
     # host = 
     # topic =
     name = models.CharField(max_length=200)
-    description = models.TextField(null=TRUE, blank=True)   # description은 없어도 된다.
+    description = models.TextField(null=True, blank=True)   # description은 없어도 된다.
     # participants = 
     updated = models.DateTimeField(auto_now=True)  # 업데이트 될때마다 시간이 바뀜
     created = models.DateTimeField(auto_now_add=True)  # 처음에 생성되는 초기값만 저장됨(수정하고 저장해도 값 변경 안됨) 

b0c30bf2c20ebdf42792e1f9443b749a298aecf2
전은형 <ooooo5h91@gmail.com>
2022-03-01 12:23:59 +0900

    auto_now와 auto_now_add의 차이점

--- base/models.py
+++ base/models.py
@@ -9,3 +9,6 @@ class Room(models.Model):
     # topic =
     name = models.CharField(max_length=200)
     description = models.TextField(null=TRUE, blank=True)   # description은 없어도 된다.
+    # participants = 
+    updated = models.DateTimeField(auto_now=True)  # 업데이트 될때마다 시간이 바뀜
+    created = models.DateTimeField(auto_now_add=True)  # 처음에 생성되는 초기값만 저장됨(수정하고 저장해도 값 변경 안됨) 

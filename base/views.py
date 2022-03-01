18c0a8c5aea14199d95afb4b268e3f204558446c
전은형 <ooooo5h91@gmail.com>
2022-03-01 19:39:52 +0900

    room 개수를 확인하는 코드 추가

--- base/views.py
+++ base/views.py
@@ -14,14 +14,17 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
     rooms = Room.objects.filter(
         Q(topic__name__icontains = q)  |
         Q(name__icontains=q) |
         Q(description__icontains=q)
         )
-    topics = Topic.objects.all()
     
-    context = {'rooms' : rooms, 'topics' : topics}
+    topics = Topic.objects.all()
+    room_count = rooms.count()   # len보다 속도가 빠른 count사용
+    
+    context = {'rooms' : rooms, 'topics' : topics, 'room_count' : room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):

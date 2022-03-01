3f4c72b5984e34e28e7107757c7217e20546e0f6
전은형 <ooooo5h91@gmail.com>
2022-03-01 12:20:00 +0900

    DB테이블 생성 중
    - 파라미터로 null=True하면 없어도 된다.
    - 기본값은 null=False로 되어있음

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
+# DB 테이블을 생성하는 곳
+
+class Room(models.Model):
+    # host = 
+    # topic =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=TRUE)   # description은 없어도 된다.

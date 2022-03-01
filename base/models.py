e52349ea615698a9dc896757ca40d80747a32c18
전은형 <ooooo5h91@gmail.com>
2022-03-01 14:15:23 +0900

    Room과 Topic의 관계 설정
    - SET_NULL은 topic이 삭제되어도 남아있게 처리해줌

--- base/models.py
+++ base/models.py
@@ -13,7 +13,7 @@ class Topic(models.Model):
 
 class Room(models.Model):
     # host = 
-    # topic =
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)   # description은 없어도 된다.
     # participants = 

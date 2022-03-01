40ed10b6c6f7eaf78a00c51c7355840cde0ae731
전은형 <ooooo5h91@gmail.com>
2022-03-01 15:25:08 +0900

    수정하는 링크 연결

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [   # path(경로, view의 기능, 참조할 이름)
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>', views.updateRoom, name='update-room'),
 ]

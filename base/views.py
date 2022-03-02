f47ad79cd50352029f2f8d86fba12e5ed1b89213
전은형 <ooooo5h91@gmail.com>
2022-03-02 15:17:47 +0900

    UserCreationForm을 이용해 회원가입 화면 생성

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -50,8 +51,8 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    page = 'register'
-    return render(request, 'base/login_register.html')
+    form = UserCreationForm()
+    return render(request, 'base/login_register.html', {'form' : form})
 
 
 def home(request):

1c3b040e9114ebe8bfd94d799d251013bf3647d6
전은형 <ooooo5h91@gmail.com>
2022-03-02 16:17:51 +0900

    댓글을 작성한 사람만 삭제 가능

--- base/urls.py
+++ base/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [   # path(경로, view의 기능, 참조할 이름)
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>', views.deleteRoom, name='delete-room'),
+    path('delete-message/<str:pk>', views.deleteMessage, name='delete-message'),
 ]

a42890fb7db2297fe3c80195a306afa9d4d502c2
전은형 <ooooo5h91@gmail.com>
2022-03-02 15:53:44 +0900

    다대다관계 설정하는 법
    - ManyToManyField
    - makemigrations 실행
    - migrate 실행

--- base/models.py
+++ base/models.py
@@ -16,7 +16,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)   # description은 없어도 된다.
-    # participants = 
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)  # 업데이트 될때마다 시간이 바뀜
     created = models.DateTimeField(auto_now_add=True)  # 처음에 생성되는 초기값만 저장됨(수정하고 저장해도 값 변경 안됨) 
     

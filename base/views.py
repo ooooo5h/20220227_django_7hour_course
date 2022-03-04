c3e04270db3c0ca8123baf6503945c313a799184
전은형 <ooooo5h91@gmail.com>
2022-03-04 15:42:54 +0900

    최근 활동 내역 연결 완료

--- base/views.py
+++ base/views.py
@@ -207,4 +207,4 @@ def topicsPage(request):
 
 def activityPage(request):
     room_messages = Message.objects.all()
-    return render(request, 'base/activity.html', {})
+    return render(request, 'base/activity.html', {'room_messages' : room_messages})

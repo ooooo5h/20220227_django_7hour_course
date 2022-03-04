924dfabfa0ea5464e259024b4c87ccba999e1956
전은형 <ooooo5h91@gmail.com>
2022-03-04 18:50:20 +0900

    내 정보 수정부터 업데이트함

--- base/forms.py
+++ base/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, User
 
 class RoomForm(ModelForm):
@@ -11,4 +12,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['avatar', 'name', 'username', 'email', 'bio']

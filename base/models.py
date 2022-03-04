1282fcfba3873364d34605a0e42ee009a4786402
전은형 <ooooo5h91@gmail.com>
2022-03-04 18:35:26 +0900

    python -m pip install Pillow

--- base/models.py
+++ base/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
-    # avatar = 
+    avatar = models.ImageField()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
